c4cd72d001b81dc83f9b9023341dec3847b4c9f6
Diego Berrocal <cestdiego@gmail.com>
2015-07-24 14:21:04 -0500

    yeap

--- .config/i3status/custom_bar.sh
+++ .config/i3status/custom_bar.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 i3status | while :
 do
     read line
-    echo $(~/.config/i3status/custom-segments.py $line)
+	  MPCSTAT=`mpc | grep -v '^volume:'`
+	  if [ -z "${MPCSTAT}" ]; then
+		    echo "$line" || exit 1
+	  else
+		    MPCSTAT=`echo "${MPCSTAT}" | sed 's,  *, ,g; 1h;1d;2G' | paste -d' ' -s -`
+		    echo "${MPCSTAT} $line" || exit 1
+	  fi
 done
