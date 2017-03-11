76c0aafecb6738a1c2788f63a6c3f928aa0399af
Diego Berrocal <cestdiego@gmail.com>
2017-03-11 15:19:27 -0500

    Add lua configuration files to my hammerspoon

---
+++ .hammerspoon/reload-config.lua
@@ -0,0 +1,14 @@
+-- Reload Automatically
+function reloadConfig(files)
+  doReload = false
+  for _,file in pairs(files) do
+    if file:sub(-4) == ".lua" and file:sub(2) ~= ".#" then
+      doReload = true
+    end
+  end
+  if doReload then
+    hs.reload()
+  end
+end
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
